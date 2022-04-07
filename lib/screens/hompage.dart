45f7ede6714b916161098b84355a4511358522a1
ikgun <ikbalgun98@gmail.com>
2022-04-07 12:59:16 +0200

    hi

---
+++ lib/screens/hompage.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:after_school/widgets/login_form.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 24.0,
+                vertical: 24.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Studentpubar',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 130),
+                    child: Column(
+                      children: [
+                        LoginForm(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Events',
+          ),
+        ],
+      ),
+    );
+  }
+}
