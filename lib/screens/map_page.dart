eee0a55c5ba2348b93c1829d708ffa0368c2f8d7
ahmed-shameel <82181516+ahmed-shameel@users.noreply.github.com>
2022-04-28 00:07:55 +0200

    Ändrat namn på titeln. Ändrat namn på screens. Lagt till kartan.

--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class map_page extends StatelessWidget {
-  const map_page({Key? key}) : super(key: key);
+class MapPage extends StatefulWidget {
+  const MapPage({Key? key}) : super(key: key);
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  static const minZoom = 9.0;
+  static const maxZoom = 30.0;
+  static const _initialCameraPosition = CameraPosition(
+      target: LatLng(59.3293, 18.0686),
+      zoom: minZoom);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
+      body: GoogleMap(
+        minMaxZoomPreference: MinMaxZoomPreference(minZoom, maxZoom),
+        initialCameraPosition: _initialCameraPosition,
+        myLocationEnabled: true,
+      ),
     );
   }
 }
