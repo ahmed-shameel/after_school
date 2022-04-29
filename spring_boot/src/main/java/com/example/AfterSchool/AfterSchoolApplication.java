42a15afe3f1509fc21b5599bb180e7830159a43d
ahmed-shameel <82181516+ahmed-shameel@users.noreply.github.com>
2022-04-29 11:36:17 +0200

    spring-boot update

--- spring_boot/src/main/java/com/example/AfterSchool/AfterSchoolApplication.java
+++ spring_boot/src/main/java/com/example/AfterSchool/AfterSchoolApplication.java
@@ -2,10 +2,9 @@ package com.example.AfterSchool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AfterSchoolApplication extends SpringBootServletInitializer {
+public class AfterSchoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AfterSchoolApplication.class, args);
