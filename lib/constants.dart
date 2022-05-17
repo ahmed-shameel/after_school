60e299e7912a05cbfa02dc5714ff0f23ca4572f5
cvmlundqvist <103148890+cvmlundqvist@users.noreply.github.com>
2022-05-17 16:53:52 +0200

    Appbar color change

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFFA3DCCB); // background color of all screens
-const appBarColor = Colors.white;
+const appBarColor = Color(0xFFD4EAE3);  //förslag enligt designgruppen, tidigare var Colors.white
 const iconColor = Colors.black;
--- lib/screens/friend_feed/friend_feed_screen.dart
+++ lib/screens/friend_feed/friend_feed_screen.dart
@@ -7,6 +7,15 @@ class FriendsFeedScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryColor,
+      appBar: AppBar(
+        backgroundColor: appBarColor,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Friends',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      ),
       body: Body(),
     );
   }
