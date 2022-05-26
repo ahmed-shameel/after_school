c370a51002bf84ae89e6d1f1c973eefaf17a8d68
cvmlundqvist <103148890+cvmlundqvist@users.noreply.github.com>
2022-05-26 10:52:43 +0200

    newPub update

--- lib/screens/pub/components/newPub.dart
+++ lib/screens/pub/components/newPub.dart
@@ -1,33 +1,29 @@
-import 'dart:convert';
 import "package:http/http.dart" as http;
-import 'package:after_school/screens/event/components/event.dart';
+
 import '../../review/components/review.dart';
 
 
 class newPub{
-  String pubName;
-//  String openingHours;
-//  bool? open;
-//  double? rating;
-//  String address;
-//  String requirements;
-//  String description;
-//  String prices;
-//  String entry;
-//  String coordinates;
-//  List<String> images = <String>[];
-//  List<Event> events = <Event>[];
-//  List<Review> reviews = <Review>[];
+  String name;
+  String? openingHours;
+  bool? open;
+  String? description;
+  String? coordinates;
+  double? rating;
+  String? address;
+  String? university;
+//  List<Review>? reviews = <Review>[];
 
   newPub({
-    required this.pubName,
-//    required this.openingHours,
-//    required this.address,
-//    required this.coordinates,
-//    required this.requirements,
-//    required this.description,
-//    required this.prices,
-//    required this.entry,
+    required this.name,
+    this.openingHours,
+    this.open,
+    this.description,
+    this.coordinates,
+    this.rating,
+    this.address,
+    this.university,
+//    this.reviews,
   });
  // void setHours(String hours) {
 //    openingHours = hours;
@@ -37,11 +33,19 @@ class newPub{
 
     return newPub(
 
-      pubName: json['pubName'],
- //     openingHours: json['openingHours'],
- //     address: json['adress'],
- //     description: json['description'],
- //     coordinates: json['coordinates'],
+      name: json['name'],
+      openingHours: json['openingHours'],
+      open: json['open'],
+      description: json['description'],
+      coordinates: json['coordinates'],
+      rating: json['rating'],
+      address: json['adress'],
+      university: json['university'],
+ //     reviews: json['reviews'],
+
+
+
+
 
     );
   }
